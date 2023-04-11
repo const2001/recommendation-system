e3fd0d6fb06e74c07b2fce25f4164baa48ee45be
Konstantinos Stavrianos <konos2001@hotmail.com>
2023-04-11 16:25:56 +0300

    tt

--- recommendation_gen.py
+++ recommendation_gen.py
@@ -25,34 +25,6 @@ def recommend_coupons(user, coupons, events):
     return sorted(recommended_coupons, key=lambda x: x["odds"], reverse=True)
 
 
-# User data
-users = [
-    {"user_id": 1, "birth_year": 1990, "country": "Italy", "sport_pref": "Football",
-        "currency": "EUR", "registration_date": "2022-03-01T00:00:00", },
-    {"user_id": 2, "birth_year": 1985, "country": "Germany", "sport_pref": "Football",
-        "currency": "EUR", "registration_date": "2022-03-01T00:00:00"},
-    {"user_id": 3, "birth_year": 1995, "country": "Spain", "sport_pref": "Football",
-        "currency": "EUR", "registration_date": "2022-03-01T00:00:00"}
-]
 
-# Event data
-events = [
-    {"event_id": "E001", "league": "Premier League", "sport": "Football", "country": "England", "begin_timestamp":
-        "2022-04-10 15:00:00+00:00", "end_timestamp": "2022-04-10 17:00:00+00:00", "participants": ["Liverpool", "Chelsea"]},
-    {"event_id": "E002", "league": "La Liga", "sport": "Football", "country": "Spain", "begin_timestamp": "2022-04-10 20:00:00+02:00",
-        "end_timestamp": "2022-04-10 22:00:00+02:00", "participants": ["Real Madrid", "Barcelona"]},
-    {"event_id": "E003", "league": "Serie A", "sport": "Football", "country": "Italy", "begin_timestamp": "2022-04-12 21:00:00+02:00",
-        "end_timestamp": "2022-04-12 23:00:00+02:00", "participants": ["Juventus", "AC Milan"]}
-]
-
-# Coupon data
-coupons = [
-    {"coupon_id": "C001", "selections": [{"event_id": "E001", "odds": 2.0}],
-        "stake": 10.0, "timestamp": "2022-04-08T09:30:00", "user_id": 1},
-    {"coupon_id": "C002", "selections": [{"event_id": "E002", "odds": 1.5}, {
-        "event_id": "E003", "odds": 2.0}], "stake": 5.0, "timestamp": "2022-04-08T10:30:00", "user_id": 2},
-    {"coupon_id": "C003", "selections": [{"event_id": "E003", "odds": 2.5}],
-        "stake": 20.0, "timestamp": "2022-04-08T11:30:00", "user_id": 3},
-]
 
 # print(recommend_coupons(1, coupons, events)) # Outputs [{'coupon_id': 'C001', 'selections': [{'event_id': 'E001', 'odds': 2.0}], 'stake': 10.0, 'timestamp': '2022-04-08T09:30:00', 'user_id': 1, 'odds': 2.0}]
